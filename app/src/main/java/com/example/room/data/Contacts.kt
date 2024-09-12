5a159ba13f5a28facded177a25d2f2cb44925bcd
Sagnik Mukherjee <mukherjee.sagnik17@gmail.com>
2024-09-12 09:36:31 +0530

    data class added

--- app/src/main/java/com/example/room/data/Contacts.kt
+++ app/src/main/java/com/example/room/data/Contacts.kt
@@ -2,10 +2,17 @@ package com.example.room.data
 
 import android.health.connect.datatypes.ExerciseRoute.Location
 import android.provider.ContactsContract.CommonDataKinds.Email
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity(tableName = "contacts") // Entity Annotation
 data class Contact(
-    var name: String,
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "ID") var id: Int? = null, // Primary Key Annotation
+    @ColumnInfo(name = "Name") var name: String,
     var location: String,
     var number: String,
     var email: Email
 )
+
