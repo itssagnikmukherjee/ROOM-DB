06325591031b051827a5a5234885a91b7203d8f3
Sagnik Mukherjee <mukherjee.sagnik17@gmail.com>
2024-09-16 20:30:51 +0530

    update++

---
+++ app/src/main/java/com/example/room/presentation/screens/AddEditContactScreen.kt
@@ -0,0 +1,17 @@
+package com.example.room.presentation.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AddEditContactScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+}
