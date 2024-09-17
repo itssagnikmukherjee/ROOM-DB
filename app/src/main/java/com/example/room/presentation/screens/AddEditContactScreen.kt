3d977caaeb0d0a52423108a5bf64a779f81d2e55
Sagnik Mukherjee <mukherjee.sagnik17@gmail.com>
2024-09-17 13:09:17 +0530

    edit screen added

--- app/src/main/java/com/example/room/presentation/screens/AddEditContactScreen.kt
+++ app/src/main/java/com/example/room/presentation/screens/AddEditContactScreen.kt
@@ -2,16 +2,36 @@ package com.example.room.presentation.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
 
+@Preview(showBackground = true)
 @Composable
 fun AddEditContactScreen() {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        var name by remember { mutableStateOf("") }
+        var email by remember { mutableStateOf("") }
+        var phone by remember { mutableStateOf("") }
+        Column {
+            OutlinedTextField(value = name, onValueChange = {name=it})
+            OutlinedTextField(value = email, onValueChange = {email=it})
+            OutlinedTextField(value = phone, onValueChange = {phone=it})
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Save Contact")
+            }
+        }
     }
 }
